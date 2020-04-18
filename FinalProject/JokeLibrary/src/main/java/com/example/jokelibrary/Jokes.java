90899f29f844f4eb95c4f646e46a4f21abea64b2
Joel Oguntoye <joeloguntoye@gmail.com>
2020-04-18 18:38:14 +0100

    minor fixes

--- FinalProject/JokeLibrary/src/main/java/com/example/jokelibrary/Jokes.java
+++ FinalProject/JokeLibrary/src/main/java/com/example/jokelibrary/Jokes.java
@@ -7,19 +7,19 @@ public class Jokes {
 
 
     public  static final String[] JokeString = {"Teacher: How did you find that doctor was fake?\n" +
-                "John: Because he had a good handwriting!\n" +
-                "\n",
-                "Jack: Hey, man! Please call me a taxi.\n" +
-                        "segun: Yes, sir. You are a taxi. ",
-                "A: Do you want to hear a dirty joke?\n" +
-                        "B: Ok\n" +
-                        "A: A white horse fell in the mud. ",
-                "In a restaurant:\n" +
-                        "\n" +
-                        "Customer: Waiter, waiter! There is a frog in my soup!!!\n" +
-                        "Waiter: Sorry, sir. The fly is on vacation. ",
-                "Bank Teller: How do you like the money?\n" +
-                        "English Student: I like it very much. "};
+            "John: Because he had a good handwriting!\n" +
+            "\n",
+            "Jack: What is the carpet telling the floor?\n" +
+                    "segun: I've got you covered",
+            "A: Do you want to hear a dirty joke?\n" +
+                    "B: Ok\n" +
+                    "A: A white horse fell in the mud. ",
+            "In a restaurant:\n" +
+                    "\n" +
+                    "Customer: Waiter, waiter! There is a frog in my soup!!!\n" +
+                    "Waiter: Sorry, sir. The fly is on vacation. ",
+            "Teacher: What do you call a two-humped camel?\n" +
+                    "English Student: Pregnant!!"};
 
     public static String getJokes() {
         return JokeString[new Random().nextInt(JokeString.length)];
