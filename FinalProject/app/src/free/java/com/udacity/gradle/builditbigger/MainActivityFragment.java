021e364f3ad2d997d48f39859aa85de47e7767df
Joel Oguntoye <joeloguntoye@gmail.com>
2020-04-18 16:42:14 +0100

    Adding flavors and asyncTask test

---
+++ FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,92 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import com.example.jokedisplaylibrary.JokeActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private ProgressBar mLoadingIndicator;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        //Initialize mobile ad
+        MobileAds.initialize(getContext(), getString(R.string.admob_app_id));
+
+        AdView mAdView = root.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest
+                .Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
+        Button button = root.findViewById(R.id.button_joke);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivityFragment.this.getJoke();
+            }
+        });
+
+        mLoadingIndicator = root.findViewById(R.id.pb_loading_indicator);
+        return root;
+    }
+
+    //This method retrieves a joke string from AsyncTask and open activity
+    private void getJoke() {
+        EndpointsAsyncTask.getInstance(new OnJokeRetrieve() {
+            @Override
+            public void onRetrieveJokeStart() {
+                //Show progress bar
+                mLoadingIndicator.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onRetrieveJokeFinish(@Nullable String result) {
+                //Hide progress bar
+                mLoadingIndicator.setVisibility(View.GONE);
+                // If result is empty, show toast ith message
+                if (TextUtils.isEmpty(result)) {
+                    Toast.makeText(getActivity(), "No jokes retrieved", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                launchJokeDisplayActivity(result);
+            }
+        });
+    }
+
+    // This method launches the joke display activity from Android library
+    private void launchJokeDisplayActivity(@Nullable String result) {
+        final Intent intent = new Intent(getActivity(), JokeActivity.class);
+        intent.putExtra(JokeActivity.JOKE_TAG, result);
+        startActivity(intent);
+    }
+
+}
---
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -0,0 +1,61 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
+
+class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
+
+    private static final String TAG = EndpointsAsyncTask.class.getSimpleName();
+    private static MyApi myApiService = null;
+    private final OnJokeRetrieve mCallback;
+
+    private EndpointsAsyncTask(OnJokeRetrieve callback) {
+        mCallback = callback;
+    }
+
+    public static void getInstance(OnJokeRetrieve listener) {
+        new EndpointsAsyncTask(listener).execute();
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        mCallback.onRetrieveJokeStart();
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        if (myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(abstractGoogleClientRequest -> abstractGoogleClientRequest.setDisableGZipContent(true));
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        try {
+            return myApiService.getJoke().execute().getData();
+        } catch (IOException e) {
+            Log.d(TAG, "AsyncTaskError: " + e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        mCallback.onRetrieveJokeFinish(result);
+    }
+
+
+}
